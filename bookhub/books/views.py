b19d9c27facb0a73edd38fadd914839dd167056e
Suryansh Sharma <suryansharma09@gmail.com>
2024-12-18 20:07:33 +0530

    book search view implemented

--- bookhub/books/views.py
+++ bookhub/books/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+import requests
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class BookSearchView(APIView):
+    """🔍 Allows users to search for books on Library Genesis"""
+
+        def get(self, request):
+        query = request.query_params.get('q', '')
+        if not query:
+            return Response({'error': 'Search query is required'}, status=400)
+        
+        url = f'http://libgen.is/search.php?req={query}&open=0&res=100&view=simple&phrase=1&column=def'
+        response = requests.get(url)
+        
+        # Extract book details (this might require parsing the response if it's HTML)
+        # For now, we're assuming we get a JSON response
+        # Sample response handling (You may need BeautifulSoup to parse HTML)
+        books = []  # Placeholder: This would be populated by scraping the website
+        for book in books:
+            books.append({
+                'title': book['title'],
+                'author': book['author'],
+                'download_url': book['download_url'],
+                'file_size': book['size'],
+                'file_type': book['type']
+            })
+        
+        return Response({'results': books}, status=200)
--- bookhub/users/permissions.py
+++ bookhub/users/permissions.py
@@ -1,9 +1,10 @@
 
-# we'll restrict book upload functionality to users with the "Author"
+# we'll restrict book upload functionality to users with the "Admin"
 # role and allow book reading for "Readers".
 
 from rest_framework.permissions import BasePermission
 
-class IsAuthor(BasePermission):
+class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role == 'author'
+        return request.user.role == 'admin'
+
