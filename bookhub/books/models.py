293505721231748ef9f86db0be6be002b64ef5e4
Suryansh Sharma <suryansharma09@gmail.com>
2024-12-18 22:11:44 +0530

    book model added

--- bookhub/books/models.py
+++ bookhub/books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Book(models.Model):
+    """📚 Model for storing downloaded book information"""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    file_path = models.FileField(upload_to='books/epub/')
+    created_at = models.DateTimeField(auto_now_add=True)
