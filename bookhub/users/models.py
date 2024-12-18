f00e0e57a17f6a21aae7890feb1ba905c6a90896
Suryansh Sharma <suryansharma09@gmail.com>
2024-12-18 17:33:58 +0530

    custom user manager  created

--- bookhub/users/models.py
+++ bookhub/users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+
+# we will create a custom user model to have more flexibility
+# (like adding roles) and customize the fields.
+
+#custom user manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self,email, password = None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email)
+        extra_fields.setdefault('is_active', True)
+        user= self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using= self._db)
+        return user
+    
+
+    def create_superuser(self, email, password= None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email,password,**extra_fields)
+        
