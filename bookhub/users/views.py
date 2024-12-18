20685e2a92461e2d10797d80357817e3e303ddd2
Suryansh Sharma <suryansharma09@gmail.com>
2024-12-18 18:31:54 +0530

    register view created for user

--- bookhub/users/views.py
+++ bookhub/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import CustomUser
+from .serializers import UserSerializer, LoginSerializer
 
-# Create your views here.
+#register views
+
+class RegisterView(APIView):
+    def post(self,request):
+        serializer= UserSerializer(data= request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({'message': "user registered successfully"}, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
