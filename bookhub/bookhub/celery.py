fda3d34e9fbb7bbf882bc4a5873d39ef4147cac0
Suryansh Sharma <suryansharma09@gmail.com>
2024-12-19 16:34:48 +0530

    configured celery for book downloading

---
+++ bookhub/bookhub/celery.py
@@ -0,0 +1,13 @@
+from celery import Celery
+import requests
+
+app = Celery('book_hub', broker='redis://localhost:6379/0')
+
+app.task
+def download_book(download_url, file_path):
+    """📥 Background task for downloading ePub books"""
+    response= requests.get(download_url, stream = True)
+    with open(file_path,'wb') as file :
+        for chunk in response.iter_content(chunk_size=81292):
+            file.write(chunk)
+    return 'Download Complete'
