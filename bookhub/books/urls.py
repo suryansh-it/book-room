1be0a88ad54c8478acbe2779c5b9a00e2b1f47d2
Suryansh Sharma <suryansharma09@gmail.com>
2024-12-20 18:38:02 +0530

    added urls to access book read view and user library

--- bookhub/books/urls.py
+++ bookhub/books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookSearchView, BookDownloadView
+from .views import BookSearchView, BookDownloadView, BookReadView
 
 urlpatterns = [
     path('search/', BookSearchView.as_view(), name='book-search'),  # New search URL
-    path('download/', BookDownloadView.as_view(), name='book-download'),  
+    path('download/', BookDownloadView.as_view(), name='book-download'),
+    path('books/read/<int:book_id>/', BookReadView.as_view(), name='read-book'),  
 ]
--- bookhub/users/urls.py
+++ bookhub/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, UserLibraryView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('books/library/', UserLibraryView.as_view(), name='user_library'),
+
 ]
