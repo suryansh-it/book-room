4536e4d74e2806acca5e1d182a1d201d8bb02048
Suryansh Sharma <suryansharma09@gmail.com>
2024-12-21 10:48:18 +0530

    created extracting functioin to help in reading the epub

--- bookhub/users/urls.py
+++ bookhub/users/urls.py
@@ -4,6 +4,6 @@ from .views import RegisterView, LoginView, UserLibraryView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('books/library/', UserLibraryView.as_view(), name='user_library'),
+    path('books/library/', UserLibraryView.as_view(), name='user-library'),
 
 ]
