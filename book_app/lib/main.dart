ab3b618d41f1809dbb236166721b95bdf88df535
Suryansh Sharma <suryansharma09@gmail.com>
2024-12-29 23:04:42 +0530

    modifiesd main.dart

--- book_app/lib/main.dart
+++ book_app/lib/main.dart
@@ -1,29 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'providers/auth_provider.dart';
-import 'screens/auth/login_screen.dart';
 import 'screens/home_screen.dart';
+import 'screens/auth/login_screen.dart';
+import 'screens/auth/signup_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(BookApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BookApp extends StatelessWidget {
+  const BookApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-      ],
-      child: MaterialApp(
-        title: 'Book App',
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home: LoginScreen(),
-        routes: {
-          '/home': (_) => HomeScreen(),
-        },
+    return MaterialApp(
+      title: 'Book App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      // Define routes for navigation
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/login': (context) => LoginScreen(),
+        '/signup': (context) => SignupScreen(),
+      },
     );
   }
 }
