b80adcd0773da9b740668367c670a8720ccbcf00
Suryansh Sharma <suryansharma09@gmail.com>
2024-12-29 18:38:55 +0530

    created admin

--- bookhub/users/admin.py
+++ bookhub/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+# Register your CustomUser model to appear in the Django Admin panel
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'role', 'is_staff', 'is_active')  # Display relevant fields
+    search_fields = ('email', 'role')  # Allow searching by email and role
+    list_filter = ('is_staff', 'is_active')  # Filter by staff or active status
+
+admin.site.register(CustomUser, CustomUserAdmin)
